3961511c401f0f26dfb9bc6fa3cf207aed792da4
Arif Uddin <arifunctg@gmail.com>
2024-07-23 14:08:36 +0600

    style: update bg color of login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
       <Card className="w-[350px]">
         <CardHeader>
           <CardTitle>Login</CardTitle>
