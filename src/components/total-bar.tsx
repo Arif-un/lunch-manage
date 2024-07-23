c13f874e73ea9e41a559f7b3ed34405b90415eee
Arif Uddin <arifunctg@gmail.com>
2024-07-23 14:11:30 +0600

    fix: total bar style and fetch price

--- src/components/total-bar.tsx
+++ src/components/total-bar.tsx
@@ -1,15 +1,18 @@
+import { getMealPrice } from '../app/api/meals/[userId]/route'
 import PriceSetter from './price-setter'
 
-export default function TotalBar({ price, quantity }: { price: number; quantity: number }) {
+export default async function TotalBar({ quantity, date }: { quantity: number; date: string }) {
+  const price = await getMealPrice(date)
+
   const totalFormatted = (price * quantity).toFixed(0)
   return (
-    <div className="flex  items-center justify-between rounded-md border p-2 md:w-4/6">
+    <div className="flex items-center justify-between rounded-md border p-2 px-3">
       <span className="text-slate-400">Total</span>
 
       <div className="flex items-center gap-1 text-sm font-normal">
         <span>{quantity}</span>
         <span>X</span>
-        <PriceSetter price={price} />
+        <PriceSetter price={price} date={date} />
         <span>=</span>
         <span className="font-semibold text-slate-600">{totalFormatted}</span>
       </div>
