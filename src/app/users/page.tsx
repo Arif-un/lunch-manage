702fc4aac00ba43245206fb18a02126531e5b839
Arif Uddin <arifunctg@gmail.com>
2024-07-23 14:07:07 +0600

    style: update page style

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -6,7 +6,7 @@ export default async function Users() {
   return (
     <ContentWrapper>
       <main className="flex min-h-screen flex-col items-center bg-slate-50">
-        <div className="w-11/12 md:w-4/6 ">
+        <div className="w-10/12 md:w-3/6 ">
           <div className="my-3 flex items-center gap-3">
             <NavButton />
             <h2 className="font-semibold">Users</h2>
