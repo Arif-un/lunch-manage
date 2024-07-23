d72cc66dfe437294350a345219f2d6162274d251
Arif Uddin <arifunctg@gmail.com>
2024-07-23 14:09:30 +0600

    fix: meal price fetch api

--- src/app/api/meals/prices/route.ts
+++ src/app/api/meals/prices/route.ts
@@ -1,24 +1,10 @@
-import { eq, sql } from 'drizzle-orm'
 import { type NextRequest } from 'next/server'
 
 import db from '@/src/lib/db/connection'
 import MealPrices from '@/src/lib/db/schema/MealPrices'
 
-export async function GET(request: NextRequest) {
-  const mealPrices = await Promise.all([fetchAllMealPrices(), getMealPriceByDate(date)])
-  // const getTodaysMealPrice =
-  return Response.json({ mealPrices })
-}
+export async function GET(_request: NextRequest) {
+  const prices = await db.select().from(MealPrices)
 
-async function fetchAllMealPrices() {
-  return db.select().from(MealPrices)
-}
-
-async function getMealPriceByDate(date: string) {
-  const [todaysPrice] = await db
-    .select({ price: MealPrices.price })
-    .from(MealPrices)
-    .where(eq(sql`date(${MealPrices.created_at})`, sql`date(${date})`))
-    .limit(1)
-    .execute()
+  return Response.json({ success: true, prices })
 }
