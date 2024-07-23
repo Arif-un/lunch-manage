3e4ca125e01bb6aa92843387639f8b416343266f
Arif Uddin <arifunctg@gmail.com>
2024-07-23 12:59:55 +0600

    feat: added overlay to main content while sidebar open

--- src/components/content-wrapper.tsx
+++ src/components/content-wrapper.tsx
@@ -1,14 +1,29 @@
 'use client'
 
-import { useAtomValue } from 'jotai'
+import { useAtom } from 'jotai'
 
 import userPreferenceAtom from '../global-states/user-preference-atom'
 import { cn } from '../lib/utils'
 
 export default function ContentWrapper({ children }: { children: React.ReactNode }) {
-  const { isOpenSidebar } = useAtomValue(userPreferenceAtom)
+  const [{ isOpenSidebar }, setSideBar] = useAtom(userPreferenceAtom)
+  const handlePanelClick = () => {
+    if (!isOpenSidebar) return
+    setSideBar(prv => ({ ...prv, isOpenSidebar: false }))
+  }
+
   return (
     <div className={`content-transform-transition ${cn(isOpenSidebar && 'content-aside')}`}>
+      {isOpenSidebar && (
+        <div
+          tabIndex={0}
+          role="button"
+          aria-label="Main panel open"
+          onKeyUp={handlePanelClick}
+          onClick={handlePanelClick}
+          className="fixed size-full"
+        />
+      )}
       {children}
     </div>
   )
