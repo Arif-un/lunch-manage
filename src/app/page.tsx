e2421a55311deea85409c64570faa97109227d58
Arif Uddin <arifunctg@gmail.com>
2024-07-23 13:06:55 +0600

    feat: meal update functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,60 @@
 import Link from 'next/link'
 
-import { Button } from '@/src/components/ui/button'
-
 import ContentWrapper from '../components/content-wrapper'
+import MealButton from '../components/meal-button'
+import MealDate from '../components/meal-date'
 import NavButton from '../components/nav-button'
 import TotalBar from '../components/total-bar'
-import { fetchUsers } from '../server/usersActions'
+import { Button } from '../components/ui/button'
+import { dateToday } from '../lib/utils'
+import { fetchUsersWithMeals } from '../server/usersActions'
 
-export default async function Home() {
-  const users = await fetchUsers()
-  const today = new Date()
-  const todayFormattedDate = today.toISOString().split('T')[0]
+export default async function Home({
+  searchParams: { date = dateToday() }
+}: {
+  searchParams: { date: string }
+}) {
+  const usersWithMeals = await fetchUsersWithMeals(date)
+
+  const quantity =
+    usersWithMeals?.reduce((acc, cur) => {
+      if (cur?.mealPrice && cur.mealPrice > 0) {
+        // eslint-disable-next-line no-param-reassign
+        acc += 1
+      }
+      return acc
+    }, 0) || 0
 
   return (
     <ContentWrapper>
-      <main className="flex min-h-screen flex-col items-center bg-background">
-        <div className="w-11/12 md:w-4/6 ">
-          <div className="my-2 flex justify-between">
-            <NavButton />
-            <Link href="/payments/create">careate</Link>
-            <div className="flex items-center gap-2">
-              <span className="text-slate-400">Date:</span>
-              <input aria-label="Date" type="date" value={todayFormattedDate} />
+      <main className="flex min-h-screen flex-col items-center bg-slate-50">
+        <div className="w-10/12 md:w-3/6 ">
+          <div className="my-3 flex justify-between">
+            <div className="flex items-center gap-3">
+              <NavButton />
+              <Button asChild variant="link" className="justify-start p-0 font-semibold">
+                <Link href={`/?date=${dateToday()}`}>Lunch</Link>
+              </Button>
             </div>
+
+            <MealDate date={date} />
           </div>
 
-          <div className="grid w-full grid-cols-2 place-content-center gap-4 border p-4">
-            {users?.map(user => (
-              <Button
-                key={user.id}
-                variant="outline"
-                className="flex h-10 items-center justify-start space-x-4 text-left"
-              >
-                <span className="size-4 rounded border border-slate-600" />
-                <span>{user.name}</span>
-              </Button>
+          <TotalBar quantity={quantity} date={date} />
+
+          <div className="mt-4 grid w-full grid-cols-2 place-content-center gap-4">
+            {usersWithMeals?.map(user => (
+              <MealButton
+                key={user?.userId}
+                name={user?.name}
+                id={user?.userId}
+                price={user?.mealPrice}
+                date={date}
+                lastUpdatedAt={user.lastUpdatedAt as string}
+                lastUpdatedBy={user.lastUpdatedBy as string}
+              />
             ))}
           </div>
-          <TotalBar />
         </div>
       </main>
     </ContentWrapper>
