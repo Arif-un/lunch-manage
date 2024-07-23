2b4b8e752a7f88d8f0409503debb2f413869cba0
Arif Uddin <arifunctg@gmail.com>
2024-07-23 14:12:10 +0600

    fix: user actions

--- src/server/usersActions.ts
+++ src/server/usersActions.ts
@@ -5,23 +5,34 @@ import { and, eq, sql } from 'drizzle-orm'
 import db from '../lib/db/connection'
 import Meals from '../lib/db/schema/Meals'
 import Users from '../lib/db/schema/Users'
-import users from '../lib/db/schema/Users'
 
-/**
- *
- */
-export async function fetchUsers(date: string) {
+export async function fetchUsersWithMeals(date: string) {
   try {
     const userResults = await db
-      .select()
+      .select({
+        userId: Users.id,
+        name: Users.name,
+        mealPrice: Meals.amount,
+        lastUpdatedBy: sql`(SELECT name FROM ${Users} WHERE id = ${Meals.updated_by})`,
+        lastUpdatedAt: sql`datetime(${Meals.updated_at})`
+      })
       .from(Users)
       .leftJoin(
         Meals,
         and(eq(Users.id, Meals.user_id), eq(sql`date(${Meals.created_at})`, sql`date(${date})`))
       )
+
     return userResults
   } catch (err) {
     if (err instanceof Error) console.error(err.stack)
+  }
+}
+
+export async function fetchUsers() {
+  try {
+    return await db.select().from(Users)
+  } catch (error) {
+    console.error(error)
   }
 }
 
@@ -33,7 +44,7 @@ export async function fetchUsers(date: string) {
  */
 export async function createUser(name: string, email: string, password: string) {
   try {
-    db.insert(users).values({ name, email, password }).run()
+    db.insert(Users).values({ name, email, password }).run()
   } catch (err) {
     if (err instanceof Error) console.error(err.stack)
   }
